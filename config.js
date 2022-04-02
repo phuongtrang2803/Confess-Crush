86f37aa3bd9d9090ea3b4005c03308cfb60dfe24
phuongtrang2803 <102839405+phuongtrang2803@users.noreply.github.com>
2022-04-02 09:30:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Nếu em theo đuổi lại anh thì sao?",
-    introTitle: 'Kiều Hải Đăng nạnh nùng, dáng ghét quá đi',
+    introTitle: 'Kiều Hải Đăng nạnh nùng, đáng ghét quá đi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi

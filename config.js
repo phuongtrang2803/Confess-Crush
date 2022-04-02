cf12d898b9f2a8209d518d2323b0ac8326776220
phuongtrang2803 <102839405+phuongtrang2803@users.noreply.github.com>
2022-04-02 09:13:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Nếu em theo đuổi lại anh thì sao?",
+    introTitle: 'Kiều Hải Đăng nạnh nùng, dáng ghét quá đi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnIntro: 'Em muốn theo đuổi anh đấy',
+    title: 'Em vẫn cứ thích anh đó',
+    desc: 'Anh có cho em theo đuổi anh lại không?',
+    btnYes: ' Ỏ oke bé',
+    btnNo: 'Hong pé ơi',
+    question: 'Vậy anh có chịu cho em theo đuổi lại không, sao anh ngốc thế em thích anh nhiều đến vậy mà anh không cảm nhận được à. Anh có gì muốn nói với em không? ',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Em biết rằng thời gian qua, tình cảm bọn mình nhạt dần cũng một phần do em. Đến đi ăn với nhau mà em cũng phải cầm điện thoại check tin nữa mà. Anh bảo với em anh hết nhẵn tình cảm, nhưng thật sự em không tin điều đó, chả phải vì em lụy tình k tin vào sự thât hay gì mà bởi vì anh là người em yêu. Anh bảo không muốn bước vào thế giới của em mt65 lần nữa vì sợ nếu 3 năm hay 7 năm sau vẫn sẽ lặp tình trạng như vậy. Nhưng anh à, mình vẫn còn trẻ mà, đó là chuyện của tương lai, vì anh không bảo với em tình trạng em đang gặp phải thì làm sao em có thể giúp anh gỡ rối được đây. Xin anh hãy đọc hết vì em phải thức khuya tới mấy ngày để làm cái website này đấy :((. Anh bảo thấy em cảm nhận mọi câu chuyện mà không có anh cũng làm anh yên lòng, anh giả vờ ngốc hay thật sự k biết vậy. Nếu như em cảm nhận mọi thứ thì còn gửi cho anh mỗi ngày làm gì vậy😂 Em có cố quên anh đấy nhưng em thật sự không làm được và em tư hỏi vì sao em lại phải quên đi một người mà em thương rất nhiều. Em chỉ muốn theo đuổi anh lại từ đầu thui. Anh có chắc anh hết cảm xúc không vì trong tin nhắn anh còn bảo anh đang rối rắm mà, nếu anh không thể bước vào thế giới của em một lần nữa, thì để cho em bước vào thế giới của anh nhé. ',
+    mess: 'Thời gian đại học của em chỉ thật sự ý nghĩa khi em được ở bên anh thôi, anh à. Không thử một lần sao biết là không thể.',
+    messDesc: 'Tối nay, em đợi anh dưới tòa của anh nha.',
+    btnAccept: 'Mập nạnh nùng',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
